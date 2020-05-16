af372be5ab4e4426417dc9390a1fe152f0670f4b
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-16 20:48:29 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Chihiro Nishiwaki on 2020/05/16.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
